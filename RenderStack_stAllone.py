e7103ed7251a3e10607032a2efbd54847e5518ba
vfxsnake <jazzjoe@gmail.com>
2016-10-25 10:30:39 -0400

    last update :wq

--- RenderStack_stAllone.py
+++ RenderStack_stAllone.py
@@ -1,35 +1,6 @@
 import sys
-
 from PySide import QtGui
-from PySide import QtCore
-
-from RenderStack import Ui_renderStack_DockWidget
-from RenderSettings import Ui_renderSettings_dockWidget
-
-
-class RenderSettings(QtGui.QDockWidget, Ui_renderSettings_dockWidget):
-
-    def __init__(self, parent=None):
-        super(RenderSettings, self).__init__(parent)
-
-        self.setupUi(self)
-        self.setWindowTitle('Render Settings')
-        self.setFeatures(QtGui.QDockWidget.DockWidgetFloatable | QtGui.QDockWidget.DockWidgetMovable)
-        self.setAllowedAreas(QtCore.Qt.RightDockWidgetArea)
-        self.show()
-
-class RenderStack(QtGui.QDockWidget, Ui_renderStack_DockWidget):
-
-    def __init__(self, parent=None):
-        super(RenderStack, self).__init__(parent)
-
-        self.setupUi(self)
-        self.setWindowTitle('MVM Render Stack')
-        self.renderSettigns = RenderSettings(self)
-        #self.addDockWidget(QtCore.Qt.RightDockWidgetArea, self.renderSettigns)
-        self.show()
-
-
+from RederStack import RenderStack
 
 
 def main():
